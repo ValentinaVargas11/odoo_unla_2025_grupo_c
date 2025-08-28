bf9989376c9c773bff460f768d37c64d9714eb5c
ValentinaVargas11 <valentinarociobelen2@gmail.com>
2025-08-28 05:28:38 -0300

    Actividades

--- real_estate/__manifest__.py
+++ real_estate/__manifest__.py
@@ -2,14 +2,18 @@
     'name': 'Inmobiliaria',
     'author': 'UNLa',
     'version': '1.0.0',
+    'summary': 'Un módulo creado en forma de practica para aprender Odoo',
     'description': 'Aplicacion para gestionar venta de propiedades',
+    'category': 'Tools',
     'depends': [
         'base'
     ],
     'data': [
+        'security/real_estate_res_groups.xml',
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
         'views/estate_menu_item.xml',
     ],
+    'installable': True,
     'application': True,
 }
