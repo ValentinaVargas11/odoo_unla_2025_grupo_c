541d0b9a1749e5b79f225e7942ced80f80cf11e8
Rodro560 <rodrigofunes.rgf560@gmail.com>
2025-09-16 13:51:09 -0300

    Modifico el manifest por funcionamiento

--- real_estate/__manifest__.py
+++ real_estate/__manifest__.py
@@ -13,6 +13,8 @@
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
         'views/estate_menu_item.xml',
+        'views/estate_property_type_views.xml',
+        'views/real_estate_menuitem.xml',
     ],
     'installable': True,
     'application': True,
