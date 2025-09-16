02d32b12d3ffa322e395d672ccdd351fb18f5fb6
Lucila Velardez <luluvelardez9@gmail.com>
2025-09-16 01:29:24 -0300

    agregue la direccion de offer en init y manifest

--- real_estate/__manifest__.py
+++ real_estate/__manifest__.py
@@ -13,6 +13,7 @@
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
         'views/estate_property_tag_views.xml',
+        'views/estate_property_offer_views.xml',
         'views/estate_menu_item.xml',
     ],
     'installable': True,
