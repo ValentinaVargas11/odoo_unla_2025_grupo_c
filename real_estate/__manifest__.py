9a80d0ff3cf7db5d72ececb47064ddbcc69ad8eb
Lucila Velardez <luluvelardez9@gmail.com>
2025-09-17 23:45:22 -0300

    adapte punto 28

--- real_estate/__manifest__.py
+++ real_estate/__manifest__.py
@@ -12,6 +12,7 @@
         'security/real_estate_res_groups.xml',
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
+        'views/estate_property_type_views.xml',
         'views/estate_menu_item.xml',
     ],
     'installable': True,
