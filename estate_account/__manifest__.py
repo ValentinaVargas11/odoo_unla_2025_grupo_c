4a62635be7bc89de5ceeec785b29acf038af7f6d
ValentinaVargas11 <valentinarociobelen2@gmail.com>
2025-10-15 00:09:56 -0300

    punto 27 hay q probarlo en el dev

--- estate_account/__manifest__.py
+++ estate_account/__manifest__.py
@@ -3,9 +3,9 @@
     'version': '1.0',
     'summary': 'Conectar propiedades con facturación',
     'category': 'Real Estate',
-    'depends': ['real_estate', 'account'], #account, real estate
-    'data': [], #'views/account_move_views.xml'
-    'installable': True,
-    'application': True,
-    'auto_install': True,
+    'depends': ['account','real_estate'],
+    'data': [
+        'views/account_move_views.xml'
+    ], 
+    'application': False,
 }
--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -1,27 +1,28 @@
-from odoo import models, Command
+from odoo import Command, fields, models
 from odoo.exceptions import UserError
-
 
 class EstateProperty(models.Model):
     _inherit = "estate.property"
 
     def action_sold(self):
+        #raise UserError("Estro al metodo del modulo estate_ acount")
         for property in self:
             self.env["account.move"].create = ({
-                'partner_id': property.buyer_id.id,  # a. partner_id = comprador
-                'move_type': 'out_invoice',  # b. move_type = "out_invoice"
-                'invoice_line_ids': [
+                "partner_id": property.buyer_id.id,  # a. partner_id = comprador
+                "move_type": "out_invoice",  # b. move_type = "out_invoice"
+                "property_id": property.id,
+                "line_ids": [
                     # c. Primera línea: nombre propiedad, cantidad 1, precio venta
                     Command.create({
-                        'name': property.name,
-                        'quantity': 1,
-                        'price_unit': property.selling_price,
+                        "name": property.name,
+                        "quantity": 1,
+                        "price_unit": property.selling_price,
                     }),
                     # c. Segunda línea: "Gastos administrativos", cantidad 1, precio 100
                     Command.create({
-                        'name': "Gastos administrativos",
-                        'quantity': 1,
-                        'price_unit': 100.0,
+                        "name": "Gastos administrativos",
+                        "quantity": 1,
+                        "price_unit": 100.0,
                     }),
                 ],
             }) 
