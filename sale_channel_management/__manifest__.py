7fc377a0b4e7803051afe265d049fe758cb9a226
materivas <matiensoriv@gmail.com>
2025-11-03 00:03:23 +0000

    fix: update manifest to remove deleted view files

--- sale_channel_management/__manifest__.py
+++ sale_channel_management/__manifest__.py
@@ -8,8 +8,6 @@
         'security/ir.model.access.csv',
         'views/sale_channel_views.xml',
         'views/sale_channel_menu_item.xml',
-        'views/sale_order_views.xml',
-        'views/stock_picking_views.xml',
         'views/sale_order_view_inherit.xml'
     ], 
     'installable': True,
